c0ed4d982606329c8d8116881111cb43c44b097f
Efe Karakus <efekarakus@gmail.com>
2011-10-26 21:33:40 -0500

    fixed the managerial window

--- main/ManagerialWindow.py
+++ main/ManagerialWindow.py
@@ -11,11 +11,11 @@ class ManagerialWindow(wx.Frame):
 		self.MainPanel = wx.Panel(self, -1)
 
 		self.vbox = wx.BoxSizer(wx.VERTICAL)
-		self.tssSlider = wx.Slider(frame, -1, 0, 0, 20, 
-			wx.DefaultPosition, (250, -1), wx.SL_AUTOTICKS | wx.SL_HORIZONTAL | wx.SL_LABELS)
+		self.tssSlider = \
+		wx.Slider(self.MainPanel, -1, 0, 0, 20, wx.DefaultPosition, (250, -1), wx.SL_AUTOTICKS | wx.SL_HORIZONTAL | wx.SL_LABELS)
 
 		self.vbox.Add(self.tssSlider, 1, wx.ALIGN_CENTRE)
-		self.MainPanel.SetSizer(vbox)
+		self.MainPanel.SetSizer(self.vbox)
 
 	def getTSSValue(self):
 		return self.tssSlider.GetValue()
--- main/main.py
+++ main/main.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 import os,sys
 import wx
+from ManagerialWindow import ManagerialWindow
 
-
-if __name__ == "__main__":
-	app = wx.App(False)
-	frame = wx.Frame(None, wx.ID_ANY, "Hello World")
-	panel = wx.Panel(frame, -1)
-	vbox = wx.BoxSizer(wx.VERTICAL)
-	slider = wx.Slider(frame, -1, 0, 0, 20, wx.DefaultPosition, (250, -1), wx.SL_AUTOTICKS | wx.SL_HORIZONTAL | wx.SL_LABELS)
-	vbox.Add(slider, 1, wx.ALIGN_CENTRE)
-	panel.SetSizer(vbox)
-	frame.Show(True)
-	app.MainLoop()
+app = wx.App(False)
+frame = ManagerialWindow(None, wx.ID_ANY, "Manager Window")
+frame.Show(True)
+frame.Centre()
+app.MainLoop()
