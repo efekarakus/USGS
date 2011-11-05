eb0c013777ab8c2b86a936c4edc5c24dddebe1fe
Efe Karakus <efekarakus@gmail.com>
2011-11-04 23:13:43 -0500

    added the skeleton for nathan and chris

--- main/model/setup.c
+++ main/model/setup.c
@@ -1,2 +1,25 @@
 #include "setup.h"
 
+/**
+ * Calls the helper functions import_hydro and setup_environmentals
+ * TODO: detailed comments
+ */
+void setup() {
+    import_hydro();
+    setup_environmentals();
+}
+
+/**
+ * TODO: Nathan and Chris
+ */
+void import_hydro() {
+
+}
+
+/**
+ * TODO: Tom and Efe
+ */
+void setup_environmentals() {
+
+}
+
--- main/model/setup.h
+++ main/model/setup.h
@@ -1,4 +1,11 @@
 #ifndef _SETUP
 #define _SETUP
 
+/* All the variables that are needed for setup */
+
+/**
+ * Calls the helper functions import_hydro and setup_environmentals
+ */
+void setup();
+
 #endif
