7454f070cb3eba1d1d09038a9469f45fa4f260b1
Efe Karakus <efekarakus@gmail.com>
2011-11-11 01:18:31 -0600

    Initialized Patches

--- main/model/setup.c
+++ main/model/setup.c
@@ -1,41 +1,93 @@
 #include "setup.h"
-#include "string.h"
-#include <stdio.h>
 
 /**
- * Reads one of the 
+ * Calls the helper functions import_hydro and setup_environmentals
+ * TODO: detailed comments
+ */
+void setup() {
+    find_map_sizes();
+    init_patches();
+    import_hydro();
+    setup_environmentals();
+}
+
+/**
+ * Opens the first hydro-map i.e 10k-map and finds the maximum pxcor and
+ * the maximum pycor, we assign these values to MAP_WIDTH and MAP_HEIGHT
+ */
+void find_map_sizes() {
+    // open the first hydro map
+    char* hydro_map = file_names[0];
+    char* path = "./data/HydroSets/";
+    int length = strlen(path) + strlen(hydro_map) + strlen(file_extension) + strlen(".txt") + 1;
+    char hydro_path[length];
+    strcpy(hydro_path, path);
+    strcat(hydro_path, hydro_map);
+    strcat(hydro_path, file_extension);
+    strcat(hydro_path, ".txt");
+     
+    FILE* file = fopen(hydro_path, "r");
+    if (file == NULL) {
+        fputs("error openning the hydro map", stderr);
+        exit(-1);
+    }
+
+    find_map_width_height(file); // find the width and height of the maps
+    fclose(file);
+}
+
+
+/**
+ * Reads a hydro_file finds the biggest pycor and pxcor and assigns them to MAP_WIDTH and MAP_HEIGHT
+ * @param hydro_file
+ */
+void find_map_width_height(FILE* hydro_file) {
+    int patch_info_size = 6;    // first line in the file, pxcor/pycor/depth/px-vector/py-vector/velocity
+    int word_size = 100;
+
+    char word[word_size];
+    word[0] = '\0';
+
+    int max_x = 0;
+    int max_y = 0;
+    int counter = 0;
+    while(fscanf(hydro_file, "%s", word) != EOF) {
+        // pxcor
+        if(counter%patch_info_size == 0) {
+            int value = atoi(word);
+            if(value > max_x)
+                    max_x = value;
+                    
+        }        
+        // pycor
+        else if(counter%patch_info_size == 1) {
+            int value = atoi(word);
+            if(value > max_y)
+                max_y = value;
+        }
+        counter++;
+    }
+    MAP_WIDTH = max_x;
+    MAP_HEIGHT = max_y;
+}
+
+/**
+ * Creates the 2D array of patches
  */
 void init_patches() {
-
+    // initialize the patches
+    int row = 0;
+    patches = malloc(MAP_WIDTH*sizeof(patch*));
+    for(row = 0; row < MAP_WIDTH; row++) {
+        patches[row] = malloc(MAP_HEIGHT*sizeof(patch));
+    }
 }
 
 /**
  * TODO: Nathan and Chris
  */
 void import_hydro() {
-    // Read the files
-    int i, j;
-    char file[256];
-    FILE* pFile;
-    char* str;
-    double value;
-    // Append the extension to each file
-    for(i = 0;i < 10; i++)
-    {
-        strcpy(file, file_name[i]);
-        strcat(file, file_extension);
-        pFile = fopen(file, "r");
-        
-	// Skip the file layout
-        for(j = 0; j < 6; j++){
-            fscanf(pFile, "%s", str);
-        }
 
-        while(fscanf(pFile, "%s", str) != EOF)
-        {
-            value = atoi(str);
-        }
-    }
 }
 
 /**
@@ -45,13 +97,8 @@ void setup_environmentals()
 {
 
 }
-/**
- * Calls the helper functions import_hydro and setup_environmentals
- * TODO: detailed comments
- */
-void setup() {
-    init_patches();
-    import_hydro();
-    setup_environmentals();
+
+int main() {
+    setup();
 }
 
--- main/model/setup.h
+++ main/model/setup.h
@@ -4,6 +4,8 @@
 #include "globals.h"
 #include "patch.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char* file_names[] = {"10k-", "20k-", "30k-", "40k-", "50k-", "60k-", "70k-", "80k-", "90k-", "100k-"};
 
@@ -11,6 +13,18 @@ char* file_names[] = {"10k-", "20k-", "30k-", "40k-", "50k-", "60k-", "70k-", "8
  * Calls the helper functions import_hydro and setup_environmentals
  */
 void setup();
+
+/**
+ * Opens the first hydro-map i.e 10k-map and finds the maximum pxcor and
+ * the maximum pycor, we assign these values to MAP_WIDTH and MAP_HEIGHT
+ */
+void find_map_sizes();
+
+/**
+ * Reads a hydro_file finds the biggest pycor and pxcor and assigns them to MAP_WIDTH and MAP_HEIGHT
+ * @param hydro_file
+ */
+void find_map_width_height(FILE* hydro_file);
 
 void init_patches();
 
