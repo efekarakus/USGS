161e9614786ddc8b86542ab725ae605dc7ebd8a6
Efe Karakus <efekarakus@gmail.com>
2011-11-10 21:17:30 -0600

    test git

--- main/model/globals.h
+++ main/model/globals.h
@@ -6,8 +6,6 @@
 patch** patches; ///< 2D array of patches (pixel) objects that forms a map
 int MAP_WIDTH;  ///< the map's width
 int MAP_HEIGHT; ///< the map's height
-// 2D array of patches
-
 
 // All GUI variables
 
