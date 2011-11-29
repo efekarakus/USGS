e31e029bad175cd503d1a14ed1dcde9a6212a6e0
Nathan DeGrand <degrand1@illinois.edu>
2011-11-29 15:42:21 -0600

    Updating main files

---
+++ main/model/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/** 
+ *  * To compile the code:
+ *   * gcc -m32 -shared -I/usr/include/python2.7/ -lpython2.7 -o MainModule.so main.c
+ *    * Don't forget to cp MainModule.so ../controller
+ *     */
+#include <Python.h>
+#include "setup.h"
+
+/* Python calls this to let us initialize our module */
+void initMainModule();
+
+/* Hello World function to be called from Python */
+static PyObject* py_goCommand(PyObject* self, PyObject* args);
+
+#endif
