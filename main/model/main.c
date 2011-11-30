8d069a5fbd647ede167abb4cfc14f5e6fc00ed81
Efe Karakus <efekarakus@gmail.com>
2011-11-30 11:48:51 -0600

    fixed tabbing

--- main/model/main.c
+++ main/model/main.c
@@ -3,26 +3,26 @@
 #include "setup.c"
 /* Bind Python function names to our C functions */
 static PyMethodDef MainModule_methods[] = {
-	    {"goCommand", py_goCommand, METH_VARARGS},
-		    {NULL, NULL}
+    {"goCommand", py_goCommand, METH_VARARGS},
+    {NULL, NULL}
 };
 
 /** 
- *  * Computes the tss value and returns twice as much 
- *   * @param self: the python object calling this C function
- *    * @param args: the TSS slider value
- *     * @return 2*TSS
- *      */
+ * Computes the tss value and returns twice as much 
+ * @param self: the python object calling this C function
+ * @param args: the TSS slider value
+ * @return 2*TSS
+ */
 static PyObject* py_goCommand(PyObject* self, PyObject* args) {
-		int tss;
-			PyArg_ParseTuple(args, "i", &tss);
-				tss = 2*tss;
-				    setup();
-						return Py_BuildValue("i", tss);
+    int tss;
+    PyArg_ParseTuple(args, "i", &tss);
+    tss = 2*tss;
+    setup();
+    return Py_BuildValue("i", tss);
 }
 
 
 /* Python calls this function to let us initialize our module */
 void initMainModule() {
-		(void) Py_InitModule("MainModule", MainModule_methods);
+    (void) Py_InitModule("MainModule", MainModule_methods);
 }
