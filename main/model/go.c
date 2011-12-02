713f8454a85268317e53dc3b0c1033639c8d9dee
Tom Loboda <TomLoboda14@gmail.com>
2011-12-01 21:17:05 -0600

    Created shell for update_environmentals

--- main/model/go.c
+++ main/model/go.c
@@ -1,5 +1,63 @@
 #include "go.h"
 
-void go(){
+/**
+ * This function runs the model
+ */
+void go()
+{
+
 
 }
+
+
+/**
+ * Updates the environmental variables (Primary macrophyte routine)
+ *
+ * NOTE: NEED TO IMPLEMENT "UPDATE-HYDRO-MAP" AND "CHOOSE-HYDRO-MAP"
+ */
+void update_environmentals()
+{
+	if (hours % 24 == 0)	// Updates daily
+	{
+		update_discharge();	// Pulls a new discharge from a text file
+		//choose_hydro_map();	// Chooses a new hydro map based on the discharge
+		if (hydro_changed == 1)
+		{
+			//update_hydro_map();	// Updates a new hydro map based on the discharge
+		}
+	}
+
+	if (hours % 168 == 0)	// Updates weekly
+	{
+		update_temp();	// Updates to a new water temperature
+	}
+
+	update_par()	// Updates hourly to a new photosynthetic radiation value
+}
+
+
+/**
+ * Updates the temperature variable
+ */
+void update_temp()
+{
+	// Doesn't make sense
+}
+
+/**
+ * Updates the photosynthetic radiation variable
+ */
+void update_par()
+{
+	// Doesn't make sense
+}
+
+/**
+ * Updates the discharge variable
+ */
+void update_discharge()
+{
+
+
+}
+ */
--- main/model/go.h
+++ main/model/go.h
@@ -3,6 +3,33 @@
 
 #include "main.h"
 
-/* The go function, runs the model */
+// Variables
+long hours = 0;	//> This represents 1 tick (Should always be [0, sizeof(long)])
+
+double temp_dif = 0;	//> The percent difference from 17 degrees Celsius. (Used in update_temp
+
+
+
+/* Runs the model */
+void go();
+
+/* Updates the environmental variables (Primary macrophyte routine) */
+void update_environmentals();
+
+/**
+ * Updates the temperature variable
+ */
+void update_temp();
+
+/**
+ * Updates the photosynthetic radiation variable
+ */
+void update_par();
+
+/**
+ * Updates the discharge variable
+ */
+void update_discharge();
+
 
 #endif
