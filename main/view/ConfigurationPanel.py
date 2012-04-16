54615df6c116ea3916a07c27b0f1d40f50d92499
Efe Karakus <efekarakus@gmail.com>
2012-04-16 16:54:54 -0500

    set

--- main/view/ConfigurationPanel.py
+++ main/view/ConfigurationPanel.py
@@ -79,17 +79,28 @@ class ConfigurationPanel:
 
     def reset(self):
         """Resets the values of sliders to default values""" 
-        self.tss_slider.set(DEFAULT_TSS)
-        self.macro_base_temp.set(DEFAULT_MACRO)
-        self.gross_macro_coef.set(DEFAULT_GROSS_MACRO)
-        self.resp_macro_coef.set(DEFAULT_RESP_MACRO_COEF)
-        self.sen_macro_coef.set(DEFAULT_SEN_MACRO_COEF)
-        self.macro_mass_max.set(DEFAULT_MACRO_MASS_MAX)
-        self.macro_vel_max.set(DEFAULT_MACRO_VEL_MAX)
-        self.k_phyto.set(DEFAULT_K_PHYTO)
-        self.k_macro.set(DEFAULT_K_MACRO)
-        self.fixed_temp.set(DEFAULT_TEMP)
-        self.fixed_par.set(DEFAULT_PAR)
+        self.tss_slider.delete(0,END)
+        self.tss_slider.insert(0,DEFAULT_TSS)
+        self.macro_base_temp.delete(0,END)
+        self.macro_base_temp.insert(0,DEFAULT_MACRO)
+        self.gross_macro_coef.delete(0,END)
+        self.gross_macro_coef.insert(0,DEFAULT_GROSS_MACRO)
+        self.resp_macro_coef.delete(0,END)
+        self.resp_macro_coef.insert(0,DEFAULT_RESP_MACRO_COEF)
+        self.sen_macro_coef.delete(0,END)
+        self.sen_macro_coef.insert(0,DEFAULT_SEN_MACRO_COEF)
+        self.macro_mass_max.delete(0,END)
+        self.macro_mass_max.insert(0,DEFAULT_MACRO_MASS_MAX)
+        self.macro_vel_max.delete(0,END)
+        self.macro_vel_max.insert(0,DEFAULT_MACRO_VEL_MAX)
+        self.k_phyto.delete(0,END)
+        self.k_phyto.insert(0,DEFAULT_K_PHYTO)
+        self.k_macro.delete(0,END)
+        self.k_macro.insert(0,DEFAULT_K_MACRO)
+        self.fixed_temp.delete(0,END)
+        self.fixed_temp.insert(0,DEFAULT_TEMP)
+        self.fixed_par.delete(0,END)
+        self.fixed_par.insert(0,DEFAULT_PAR)
 
     def removemap(self):
         try:
@@ -139,99 +150,88 @@ class ConfigurationPanel:
         """Creates a slider for the TSS value."""
         row,column = (0,1)
         label = Label(self.container,text="TSS: ").grid(row=row)
-        #self.tss_slider = Scale(self.container,from_=0,to=20,orient=HORIZONTAL,resolution=1,tickinterval=10,length=200)
         self.tss_slider = Entry(self.container,width=5)
-        self.tss_slider.set(DEFAULT_TSS)
+        self.tss_slider.insert(0, DEFAULT_TSS)
         self.tss_slider.grid(row=row,column=column)
 
     def _init_macro_base_temp(self):
         """Creates a slider for the macro_base_temp."""
         row,column = (1,1) 
         label = Label(self.container,text="Macro Temperature: ").grid(row=row)
-        #self.macro_base_temp = Scale(self.container,from_=11.70,to=27.70,orient=HORIZONTAL,resolution=0.01,tickinterval=8.00,length=200)
         self.macro_base_temp = Entry(self.container,width=5)
-        self.macro_base_temp.set(DEFAULT_MACRO)
+        self.macro_base_temp.insert(0,DEFAULT_MACRO)
         self.macro_base_temp.grid(row=row,column=column)
 
     def _init_gross_macro_coef(self):
         """Creates a slider for gross_macro_coef."""
         row,column = (2,1)
         label = Label(self.container,text="Gross Macro Coef: ").grid(row=row)
-        #self.gross_macro_coef = Scale(self.container,from_=0.00,to=1.00,orient=HORIZONTAL,resolution=0.01,tickinterval=0.5,length=200)
         self.gross_macro_coef = Entry(self.container,width=5)
-        self.gross_macro_coef.set(DEFAULT_GROSS_MACRO)
+        self.gross_macro_coef.insert(0,DEFAULT_GROSS_MACRO)
         self.gross_macro_coef.grid(row=row,column=column)
 
     def _init_resp_macro_coef(self):
         """Creates a slider for resp_macro_coef."""
         row,column = (3,1)
         label = Label(self.container,text="Respiration Macro Coef: ").grid(row=row)
-        #self.resp_macro_coef = Scale(self.container,from_=0.00,to=1.00,orient=HORIZONTAL,resolution=0.01,tickinterval=0.5,length=200)
         self.resp_macro_coef = Entry(self.container,width=5)
-        self.resp_macro_coef.set(DEFAULT_RESP_MACRO_COEF)
+        self.resp_macro_coef.insert(0,DEFAULT_RESP_MACRO_COEF)
         self.resp_macro_coef.grid(row=row,column=column)
 
     def _init_sen_macro_coef(self):
         """Creates a slider for sen_macro_coef."""
         row,column = (4,1)
         label = Label(self.container,text="Senescence Macro Coef: ").grid(row=row)
-        #self.sen_macro_coef = Scale(self.container,from_=0.00,to=1.00,orient=HORIZONTAL,resolution=0.01,tickinterval=0.5,length=200)
         self.sen_macro_coef = Entry(self.container,width=5)
-        self.sen_macro_coef.set(DEFAULT_SEN_MACRO_COEF)
+        self.sen_macro_coef.insert(0,DEFAULT_SEN_MACRO_COEF)
         self.sen_macro_coef.grid(row=row,column=column)
 
     def _init_macro_mass_max(self):
         """Creates a slider for macro_mass_max."""
         row,column = (5,1)
         label = Label(self.container,text="Macro Mass Max: ").grid(row=row)
-        #self.macro_mass_max = Scale(self.container,from_=500,to=1500,orient=HORIZONTAL,resolution=25,tickinterval=500,length=200)
         self.macro_mass_max = Entry(self.container,width=5)
-        self.macro_mass_max.set(DEFAULT_MACRO_MASS_MAX)
+        self.macro_mass_max.insert(0,DEFAULT_MACRO_MASS_MAX)
         self.macro_mass_max.grid(row=row,column=column)
 
     def _init_macro_vel_max(self):
         """Creates a slider for macro_vel_max."""
         row,column = (6,1)
         label = Label(self.container,text="Macro Velocity Max: ").grid(row=row)
-        #self.macro_vel_max = Scale(self.container,from_=0.2,to=1.6,orient=HORIZONTAL,resolution=0.1,tickinterval=0.7,length=200)
         self.macro_vel_max = Entry(self.container,width=5)
-        self.macro_vel_max.set(DEFAULT_MACRO_VEL_MAX)
+        self.macro_vel_max.insert(0,DEFAULT_MACRO_VEL_MAX)
         self.macro_vel_max.grid(row=row,column=column)
 
     def _init_k_phyto(self):
         """Creates a slider for k_phyto."""
         row,column = (7,1)
         label = Label(self.container,text="K-Phyto: ").grid(row=row)
-        #self.k_phyto = Scale(self.container,from_=0.00,to=1.00,orient=HORIZONTAL,resolution=0.01,tickinterval=0.50,length=200)
         self.k_phyto = Entry(self.container,width=5)
-        self.k_phyto.set(DEFAULT_K_PHYTO)
+        self.k_phyto.insert(0,DEFAULT_K_PHYTO)
         self.k_phyto.grid(row=row,column=column)
 
     def _init_k_macro(self):
         """Creates a slider for k_macro."""
         row,column = (8,1)
         label = Label(self.container,text="K-Macro: ").grid(row=row)
-        #self.k_macro = Scale(self.container,from_=0.00,to=1.00,orient=HORIZONTAL,resolution=0.01,tickinterval=0.50,length=200)
         self.k_macro = Entry(self.container,width=5)
-        self.k_macro.set(DEFAULT_K_MACRO)
+        self.k_macro.insert(0,DEFAULT_K_MACRO)
         self.k_macro.grid(row=row,column=column)
 
     def _init_fixed_temp(self):
         """Creates a slider for the temperature."""
         row,column = (9,1)
         label = Label(self.container,text="Temperature: ").grid(row=row)
-        #self.fixed_temp = Scale(self.container,from_=0,to=30,orient=HORIZONTAL,resolution=1,tickinterval=15,length=200)
         self.fixed_temp = Entry(self.container,width=5)
-        self.fixed_temp.set(DEFAULT_TEMP)
+        self.fixed_temp.insert(0,DEFAULT_TEMP)
         self.fixed_temp.grid(row=row,column=column)
 
     def _init_fixed_par(self):
         """Creates a slider for the PAR."""
         row,column=(10,1)
         label = Label(self.container,text="Photosynthetic Radiation: ").grid(row=row)
-        #self.fixed_par = Scale(self.container,from_=0,to=2000,orient=HORIZONTAL,resolution=100,tickinterval=1000,length=200)
         self.fixed_par = Entry(self.container,width=5)
-        self.fixed_par.set(DEFAULT_PAR)
+        self.fixed_par.insert(0,DEFAULT_PAR)
         self.fixed_par.grid(row=row,column=column)
         
     def _init_reset_values(self):
